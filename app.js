a89736e00ae55e4d7fc44f64f70d170505c481a4
Bibek Adhikari <bibekadhikari2141@gmail.com>
2020-04-22 16:47:09 -0700

    Hashing

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
 require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encryption = require("mongoose-encryption");
+
+const md5 = require("md5");
+
 const app = express();
 
-console.log(process.env.SECRET);
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -23,7 +25,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encryption,{secret:process.env.SECRET, encryptedFields:["password"]});
+
 
 const User = new mongoose.model("User",userSchema);
 
@@ -43,7 +45,7 @@ app.get("/register", function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
